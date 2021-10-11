d6dcbe8c4a4f5a1fe9f3c5e822e604d3c73e293d
Tom Gregor <tgregor@ivsoftware.com>
2021-10-11 07:54:40 -0600

    Add project files.

---
+++ media-plugin-21/media-plugin-21/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.XamarinForms.Input;
+using Xamarin.Forms;
+
+namespace media_plugin_21
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// WORKS Programmatically focus the Entry control
+        /// </summary>
+        private async void OnFocusChangedEntry(object sender, Telerik.XamarinForms.Common.ValueChangedEventArgs<int> e)
+        {
+            if (e.NewValue != -1)
+            {
+                var segctl = (RadSegmentedControl)sender;
+                switch (segctl.ItemsSource.Cast<string>().ToArray()[e.NewValue])
+                {
+                    case "Focus":
+                        entry.Focus();
+                        break;
+                    case "Unfocus":
+                        entry.Unfocus();
+                        break;
+                }
+                await Task.Delay(250);
+                segctl.SelectedIndex = -1;
+            }
+        }
+
+        /// <summary>
+        /// DFW - Doesn't Factually Work
+        /// </summary>
+        private async void OnFocusChangedRRTE(object sender, Telerik.XamarinForms.Common.ValueChangedEventArgs<int> e)
+        {            
+            if(e.NewValue != -1)
+            {
+                var segctl = (RadSegmentedControl)sender;
+                switch(segctl.ItemsSource.Cast<string>().ToArray()[e.NewValue])
+                {
+                    case "Focus":
+                        richTextEditor.Focus();
+                        break;
+                    case "Unfocus":
+                        richTextEditor.Unfocus();
+                        break;
+                }
+                await Task.Delay(250);
+                segctl.SelectedIndex = -1;
+            }
+        }
+    }
+}
